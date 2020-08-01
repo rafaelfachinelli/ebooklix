9d58c291c85359cd275caec9f29892c56768013d
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-08-01 10:57:43 -0300

    Config Server Local e Online

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/categorias'
+    : 'https://ebooklix.herokuapp.com/categorias';
     fetch(URL_TOP)
     .then(async (responseServer) => {
       const response = await responseServer.json();
