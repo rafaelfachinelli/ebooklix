44260817cad83f5b0eb57bbe8fe189741fa82711
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-08-03 17:15:19 -0300

    Sugestões no campo categoria

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link, useHistory } from 'react-router-dom';
 import useForm from '../../../hooks/useForm';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
 import videosRepository from '../../../repositories/videos';
+import categoriasRepository from '../../../repositories/categorias';
 
 function CadastroVideo() {
   const history = useHistory();
+  const [categorias, setCategorias] = useState([]);
+  const categoryTitles = categorias.map(({ titulo }) => titulo);
   const { handleChange, values } = useForm({
     titulo: 'Vídeo padrão',
     url: 'https://www.youtube.com/watch?v=Iuz_jc96bQk',
     categoria: 'Front End',
   });
+
+  useEffect(() => {
+    categoriasRepository
+    .getAll()
+    .then((categoriasFromServer) => {
+      setCategorias(categoriasFromServer);
+    });
+  }, []);
 
   return (
     <PageDefault>
@@ -22,10 +33,14 @@ function CadastroVideo() {
         event.preventDefault();
         //alert('Vídeo Cadastrado com sucesso!');
 
+        const categoriaEscolhida = categorias.find((categoria) => {
+          return categoria.titulo === values.categoria;
+        });
+
         videosRepository.create({
           titulo: values.titulo,
           url: values.url,
-          categoriaId: 1,
+          categoriaId: categoriaEscolhida.id,
         })
         .then(() => {
           console.log('Cadastrou com sucesso!')
@@ -52,6 +67,7 @@ function CadastroVideo() {
           name="categoria"
           value={values.categoria}
           onChange={handleChange}
+          suggestions = {categoryTitles}
         />
 
         <Button type="submit">
--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -2,6 +2,19 @@ import config from '../config';
 
 const URL_CATEGORIES = `${config.URL_BACKEND}/categorias`;
 
+function getAll() {
+  return fetch(`${URL_CATEGORIES}`)
+    .then(async (responseServer) => {
+      if (responseServer.ok) {
+        const response = await responseServer.json();
+        return response;
+      }
+
+      throw new Error('Não foi possível acessar os dados do servidor.')
+    })
+  ;
+}
+
 function getAllWithVideos() {
   return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (responseServer) => {
@@ -17,4 +30,5 @@ function getAllWithVideos() {
 
 export default {
   getAllWithVideos,
+  getAll,
 }
