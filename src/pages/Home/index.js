185c3c85253ec2b51ea77eed3a2fb29022ca278b
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-08-03 13:21:04 -0300

    Mais vídeos adicionados para teste

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,6 +14,7 @@ function Home() {
   useEffect(() => {
     categoriasRepository.getAllWithVideos()
     .then((categoriasComVideos) => {
+      console.log(categoriasComVideos);
       setDadosIniciais(categoriasComVideos);
     })
     .catch((err) => {
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -26,7 +26,7 @@ function CadastroCategoria() {
         ...response,
       ]);
     });
-  });
+  }, []);
 
   return (
     <PageDefault>
